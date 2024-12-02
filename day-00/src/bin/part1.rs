76065d0b55f2c41c3c2d90fad22a62562f24704e
danjorgensen <upstart-deck-mace@duck.com>
2024-12-02 21:09:43 +0100

    Stop tracking target/ in day-00

--- day-00/src/bin/part1.rs
+++ day-00/src/bin/part1.rs
@@ -1,7 +1,7 @@
 fn main() {
     let input = include_str!("./input.txt");
     let output = part1(input);
-    dbg!(output);
+    dbg!(output); // Prints output to console
 }
 
 fn part1(input: &str) -> i32 {
--- day-00/target/debug/deps/day_00-8c08db9a06a51c59.d
+++
@@ -1,9 +0,0 @@
-/home/dan/app/adventofcode/day-00/target/debug/deps/libday_00-8c08db9a06a51c59.rmeta: src/main.rs Cargo.toml
-
-/home/dan/app/adventofcode/day-00/target/debug/deps/day_00-8c08db9a06a51c59.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
-# env-dep:CLIPPY_CONF_DIR
