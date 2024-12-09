a180f83ca57665cc75d6d4c14caca09092dead1f
danjorgensen <upstart-deck-mace@duck.com>
2024-12-09 21:37:35 +0100

    Setup(Day2 Part1)

--- day-02/src/bin/part1.rs
+++ day-02/src/bin/part1.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("./input.txt");
+    let output = process(input);
+    dbg!(output);
+}
+
+fn process(input: &str) -> miette::Result<String> {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() -> miette::Result<()> {
+        let input = "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9";
+
+        assert_eq!("2", process(input)?);
+        Ok(())
+    }
 }
