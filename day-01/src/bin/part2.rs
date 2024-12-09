f5793d065fa2b91bee52f3488fbd51f4888d67f0
danjorgensen <upstart-deck-mace@duck.com>
2024-12-09 19:42:05 +0100

    Part2 (setup) Test and copied part1 functions

--- day-01/src/bin/part2.rs
+++ day-01/src/bin/part2.rs
@@ -1,11 +1,26 @@
 fn main() {
-    let input = include_str!("./input1.txt");
+    let input = include_str!("./input.txt");
     let output = part2(input);
     dbg!(output);
 }
 
-fn part2(input: &str) -> String {
-    "24000".to_string()
+pub fn part2(input: &str) -> miette::Result<String> {
+    let mut left = vec![];
+    let mut right = vec![];
+
+    for line in input.lines() {
+        let mut items = line.split_whitespace();
+        left.push(items.next().unwrap().parse::<i32>().unwrap());
+        right.push(items.next().unwrap().parse::<i32>().unwrap());
+    }
+    left.sort();
+    right.sort();
+
+    let result: i32 = std::iter::zip(left, right)
+        .map(|(l, r)| (l - r).abs())
+        .sum();
+
+    Ok(result.to_string())
 }
 
 #[cfg(test)]
@@ -13,8 +28,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = part2("");
-        assert_eq!(result, "24000".to_string());
+    fn test_process() -> miette::Result<()> {
+        let input = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+
+        assert_eq!("31", part2(input)?);
+        Ok(())
     }
 }
