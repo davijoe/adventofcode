88ab15be71cbd1728ff2f1839c90754012873dc1
danjorgensen <upstart-deck-mace@duck.com>
2024-12-09 19:36:22 +0100

    Done(Day1 Part1)

--- day-01/src/bin/part1.rs
+++ day-01/src/bin/part1.rs
@@ -4,7 +4,7 @@ fn main() {
     dbg!(output);
 }
 
-pub fn part1(input: &str) -> () {
+pub fn part1(input: &str) -> miette::Result<String> {
     let mut left = vec![];
     let mut right = vec![];
 
@@ -16,7 +16,11 @@ pub fn part1(input: &str) -> () {
     left.sort();
     right.sort();
 
-    dbg!(left, right);
+    let result: i32 = std::iter::zip(left, right)
+        .map(|(l, r)| (l - r).abs())
+        .sum();
+
+    Ok(result.to_string())
 }
 
 #[cfg(test)]
@@ -24,15 +28,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = part1(
-            "3   4
+    fn test_process() -> miette::Result<()> {
+        let input = "3   4
 4   3
 2   5
 1   3
 3   9
-3   3",
-        );
-        assert_eq!(result, 11);
+3   3";
+
+        assert_eq!("11", part1(input)?);
+        Ok(())
     }
 }
