dce291f4566cd6131fc7cc75bbbafab529fa46c5
danjorgensen <upstart-deck-mace@duck.com>
2024-12-10 22:01:45 +0100

    Done(Day2 Part2)

--- day-02/src/bin/part2.rs
+++ day-02/src/bin/part2.rs
@@ -7,11 +7,48 @@ fn main() {
 fn process(input: &str) -> miette::Result<String> {
     let mut safe_reports = 0;
 
-    if safe_reports == 0 {
-        safe_reports += 2;
-    }
+    for line in input.lines() {
+        let report: Vec<i32> = line
+            .split_whitespace()
+            .filter_map(|n| n.parse().ok())
+            .collect();
 
+        if is_safe(&report) {
+            safe_reports += 1;
+            continue;
+        }
+
+        problem_dampener(&report, &mut safe_reports);
+    }
     Ok(safe_reports.to_string())
+}
+
+fn problem_dampener(report: &[i32], safe_reports: &mut i32) {
+    let mut report = report.to_vec();
+
+    for (index, _) in report.clone().iter().enumerate() {
+        let removed = report.remove(index);
+
+        if is_safe(&report) {
+            *safe_reports += 1;
+            report.insert(index, removed);
+            return;
+        }
+        report.insert(index, removed);
+    }
+}
+
+fn is_safe(report: &[i32]) -> bool {
+    // [7, 6, 4, 2, 1]
+    // [1, 2, 7, 8, 9]
+    let is_increasing = report
+        .windows(2)
+        .all(|lvl| (lvl[0] <= lvl[1]) && (lvl[1] - lvl[0] >= 1 && lvl[1] - lvl[0] <= 3));
+    let is_decreasing = report
+        .windows(2)
+        .all(|lvl| (lvl[0] >= lvl[1]) && (lvl[0] - lvl[1] >= 1 && lvl[0] - lvl[1] <= 3));
+
+    is_increasing || is_decreasing
 }
 
 #[cfg(test)]
