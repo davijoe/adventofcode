1f655289246802e5ae590da7a9dd19fa14f5c17d
danjorgensen <upstart-deck-mace@duck.com>
2024-12-10 20:40:44 +0100

    Done(Day2 Part1)

--- day-02/src/bin/part1.rs
+++ day-02/src/bin/part1.rs
@@ -5,7 +5,29 @@ fn main() {
 }
 
 fn process(input: &str) -> miette::Result<String> {
-    todo!()
+    let mut safe_reports = 0;
+
+    for line in input.lines() {
+        let report: Vec<i32> = line
+            .split_whitespace()
+            .filter_map(|n| n.parse().ok())
+            .collect();
+
+        // [7, 6, 4, 2, 1]
+        // [1, 2, 7, 8, 9]
+        let is_increasing = report
+            .windows(2)
+            .all(|lvl| (lvl[0] <= lvl[1]) && (lvl[1] - lvl[0] >= 1 && lvl[1] - lvl[0] <= 3));
+        let is_decreasing = report
+            .windows(2)
+            .all(|lvl| (lvl[0] >= lvl[1]) && (lvl[0] - lvl[1] >= 1 && lvl[0] - lvl[1] <= 3));
+
+        if is_increasing || is_decreasing {
+            safe_reports += 1;
+        }
+    }
+
+    Ok(safe_reports.to_string())
 }
 
 #[cfg(test)]
